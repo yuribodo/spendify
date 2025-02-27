504012a09a0ab657cea03312b9d3e4690b3c21c6
yuribodo <yemotalara@gmail.com>
2025-02-26 21:19:46 -0400

    feat: add a validation to see if user with that email already exists

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,7 @@
 import { UsersRepository } from "@/repositories/users-repository";
 import { User } from "@prisma/client";
 import { hash } from "bcryptjs";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 
 
 interface RegisterUseCaseRequest {
@@ -22,6 +23,12 @@ export class RegisterUseCase {
         password,
     }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
         const password_hash = await hash(password, 6)
+
+        const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+        if (userWithSameEmail ) {
+            throw new UserAlreadyExistsError()
+        }
 
         const user = await this.usersRepository.create({
             username,
