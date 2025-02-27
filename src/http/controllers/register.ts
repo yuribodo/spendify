255cd2f4a162315afbf7d0c071f93930be1689a7
yuribodo <yemotalara@gmail.com>
2025-02-26 21:41:44 -0400

    feat: create a controller to register user

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,1 +1,32 @@
-// TODO: create factory to register user
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { makeRegisterUseCase } from "@/use-cases/factories/make-register-use-case";
+import { z } from "zod";
+
+export async function register (request: FastifyRequest, reply: FastifyReply) {
+    const registerBodySchema = z.object({
+        username: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6),
+    })
+
+    const { username, email, password} = registerBodySchema.parse(request.body)
+
+    try {
+        const registerUseCase = makeRegisterUseCase()
+
+        await registerUseCase.execute({
+            username,
+            email,
+            password,
+        })
+    } catch (error) {
+        if (error instanceof UserAlreadyExistsError) {
+            return reply.status(409).send({message: error.message})
+        }
+
+        throw error
+    }
+
+    return reply.status(201).send()
+}
