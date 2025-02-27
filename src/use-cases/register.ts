4fe34c5afa13ceeeef45d616e51e337a1c9269cc
yuribodo <yemotalara@gmail.com>
2025-02-26 21:03:21 -0400

    fix: adjust name to username instead of name

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -4,7 +4,7 @@ import { hash } from "bcryptjs";
 
 
 interface RegisterUseCaseRequest {
-    name: string
+    username: string
     email: string
     password: string
 }
@@ -17,14 +17,14 @@ export class RegisterUseCase {
     constructor(private usersRepository: UsersRepository) {}
 
     async execute({
-        name,
+        username,
         email,
         password,
     }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
         const password_hash = await hash(password, 6)
 
         const user = await this.usersRepository.create({
-            username: name,
+            username,
             email,
             password_hash,
         })
