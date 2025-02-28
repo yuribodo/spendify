c21f50ed30a6e3843a2e55440b9f80b4158b3a1e
yuribodo <yemotalara@gmail.com>
2025-02-27 21:54:20 -0400

    feat: add new routes

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./controllers/register";
 import { authenticate } from "./controllers/authenticate";
+import { forgotPassword } from "./controllers/forgot-password";
+import { resetPassword } from "./controllers/reset-password";
 
 export async function appRoutes(app: FastifyInstance) {
     app.post('/users', register)
-    app.post('/users/authenticate', authenticate)
+    app.post('/session', authenticate)
+    app.post('/forgot-password', forgotPassword)
+    app.post('/reset-password', resetPassword)
 }
