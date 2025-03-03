447fe109207686865508f0fa6ce21ed333d97b36
yuribodo <yemotalara@gmail.com>
2025-03-02 22:49:46 -0400

    feat: add a controller to create expenses

---
+++ src/http/controllers/create-expense-controller.ts
@@ -0,0 +1,29 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { makeCreateExpenseUseCase } from "@/use-cases/factories/make-create-expense-use-case";
+
+export async function createExpense(request: FastifyRequest, reply: FastifyReply) {
+  const createExpenseSchema = z.object({
+    description: z.string(),
+    date: z.string(), 
+    value: z.number(),
+    categoryId: z.number(),
+    payment_method: z.string().optional(),
+  });
+
+  const { description, date, value, categoryId, payment_method } = createExpenseSchema.parse(request.body);
+  const expenseDate = new Date(date);
+  const userId = request.user.sub as string;
+
+  const createExpenseUseCase = makeCreateExpenseUseCase();
+  const { expense } = await createExpenseUseCase.execute({
+    description,
+    date: expenseDate,
+    value,
+    categoryId,
+    payment_method,
+    userId,
+  });
+
+  return reply.status(201).send(expense);
+}
