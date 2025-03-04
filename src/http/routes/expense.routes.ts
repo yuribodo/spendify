065847f3d5d3671a764a224b388e9a7be35496db
yuribodo <yemotalara@gmail.com>
2025-03-03 21:35:38 -0400

    feat: add route to get expense details

--- src/http/routes/expense.routes.ts
+++ src/http/routes/expense.routes.ts
@@ -2,9 +2,11 @@ import { FastifyInstance } from "fastify";
 import { createExpense } from "../controllers/create-expense-controller";
 import { getAllExpenses } from "../controllers/get-all-expenses";
 import { filterExpenses } from "../controllers/get-filtered-expenses";
+import { getExpenseDetails } from "../controllers/get-expense-details";
 
 export async function expenseRoutes(app: FastifyInstance) {
   app.post("/", { preHandler: [app.authenticate] }, createExpense);
   app.get("/", { preHandler: [app.authenticate] }, getAllExpenses);
   app.get("/filtered", { preHandler: [app.authenticate] }, filterExpenses);
+  app.get("/:id", { preHandler: [app.authenticate] }, getExpenseDetails);
 }
