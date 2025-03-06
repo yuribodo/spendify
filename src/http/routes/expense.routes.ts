0c669aa24656654e3404e625704a1fd684f3d795
yuribodo <yemotalara@gmail.com>
2025-03-05 21:22:46 -0400

    feat: add delete expense route

--- src/http/routes/expense.routes.ts
+++ src/http/routes/expense.routes.ts
@@ -4,10 +4,12 @@ import { updateExpense } from "../controllers/update-expense";
 import { getAllExpenses } from "../controllers/get-all-expenses";
 import { filterExpenses } from "../controllers/get-filtered-expenses";
 import { getExpenseDetails } from "../controllers/get-expense-details";
+import { deleteExpense } from "../controllers/delete-expense";
 
 export async function expenseRoutes(app: FastifyInstance) {
   app.post("/", { preHandler: [app.authenticate] }, createExpense);
   app.put("/:id", { preHandler: [app.authenticate] }, updateExpense);
+  app.delete("/:id", { preHandler: [app.authenticate] }, deleteExpense);
   app.get("/", { preHandler: [app.authenticate] }, getAllExpenses);
   app.get("/filtered", { preHandler: [app.authenticate] }, filterExpenses);
   app.get("/:id", { preHandler: [app.authenticate] }, getExpenseDetails);
