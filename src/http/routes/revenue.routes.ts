32ec8ff5a339029f0ae8a022bcd4ad42a2a4397c
yuribodo <yemotalara@gmail.com>
2025-03-06 21:19:36 -0400

    feat: add a route to filter revenues

--- src/http/routes/revenue.routes.ts
+++ src/http/routes/revenue.routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { createRevenue } from "../controllers/revenue/create-revenue";
 import { getAllRevenues } from "../controllers/revenue/get-all-revenues";
+import { filterRevenues } from "../controllers/revenue/filter-revenues";
 
 export async function revenueRoutes(app: FastifyInstance) {
   app.post("/", { preHandler: [app.authenticate] }, createRevenue);
   app.get("/", { preHandler: [app.authenticate] }, getAllRevenues);
+  app.get("/filtered", { preHandler: [app.authenticate] }, filterRevenues);
 }
