e8c89e25a90df55e81e52eee64ce2ee6ca121bec
yuribodo <yemotalara@gmail.com>
2025-03-10 21:26:32 -0400

    feat: add a new route to delete a category

--- src/http/routes/category.routes.ts
+++ src/http/routes/category.routes.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { createCategory } from "../controllers/category/create-category";
+import { deleteCategory } from "../controllers/category/delete-category";
 import { getAllCategories } from "../controllers/category/get-all-categories";
 import { updateCategory } from "../controllers/category/update-category";
 
@@ -7,4 +8,5 @@ export async function categoryRoutes(app: FastifyInstance) {
   app.post("/", { preHandler: [app.authenticate] }, createCategory);
   app.get("/", { preHandler: [app.authenticate] }, getAllCategories);
   app.put("/:id", { preHandler: [app.authenticate] }, updateCategory);
+  app.delete("/:id", { preHandler: [app.authenticate] }, deleteCategory);
 }
