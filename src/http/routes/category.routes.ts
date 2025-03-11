93f9b42a093d9eb3d107b77236754d8a98f9ab45
yuribodo <yemotalara@gmail.com>
2025-03-10 21:02:25 -0400

    feat: add a new route to update category

--- src/http/routes/category.routes.ts
+++ src/http/routes/category.routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { createCategory } from "../controllers/category/create-category";
 import { getAllCategories } from "../controllers/category/get-all-categories";
+import { updateCategory } from "../controllers/category/update-category";
 
 export async function categoryRoutes(app: FastifyInstance) {
   app.post("/", { preHandler: [app.authenticate] }, createCategory);
-  app.get("/", {preHandler: [app.authenticate]}, getAllCategories);
+  app.get("/", { preHandler: [app.authenticate] }, getAllCategories);
+  app.put("/:id", { preHandler: [app.authenticate] }, updateCategory);
 }
