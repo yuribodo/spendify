c14bf51dd6caf7f9aa1dd60deba2cfcae6c412a7
yuribodo <yemotalara@gmail.com>
2025-03-11 21:14:43 -0400

    feat: add new route to the server to export

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 import { authRoutes } from "./auth.routes";
 import { categoryRoutes } from "./category.routes";
 import { expenseRoutes } from "./expense.routes";
+import { exportRoutes } from "./export.routes";
 import { reportsRoutes } from "./reports.routes";
 import { revenueRoutes } from "./revenue.routes";
 import { userRoutes } from "./user.routes";
@@ -13,4 +14,5 @@ export async function appRoutes(app: FastifyInstance) {
     app.register(revenueRoutes, { prefix: '/revenues' });
     app.register(categoryRoutes, { prefix: '/categories' });
     app.register(reportsRoutes, { prefix: '/reports' });
+    app.register(exportRoutes, { prefix: '/export' });
 }
