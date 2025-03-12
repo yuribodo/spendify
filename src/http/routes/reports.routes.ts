ff0be7fa2df0b7975cc40c2b28cf7362367aa79f
yuribodo <yemotalara@gmail.com>
2025-03-11 20:56:10 -0400

    feat: add new routes to the server

--- src/http/routes/reports.routes.ts
+++ src/http/routes/reports.routes.ts
@@ -1,6 +1,12 @@
 import { FastifyInstance } from "fastify";
+import { getExpensesByCategoryReport } from "../controllers/reports/get-expenses-by-category-report";
 import { getFinancialSummary } from "../controllers/reports/get-financial-summary";
+import { getIncomeBySourceReport } from "../controllers/reports/get-income-by-source-report";
+import { getMonthlyExpensesReport } from "../controllers/reports/get-monthly-expenses-report";
 
 export async function reportsRoutes(app: FastifyInstance) {
     app.get("/summary", { preHandler: [app.authenticate] }, getFinancialSummary);
+    app.get("/monthly-expenses", { preHandler: [app.authenticate] }, getMonthlyExpensesReport);
+    app.get("/expenses-by-category", { preHandler: [app.authenticate] }, getExpensesByCategoryReport);
+    app.get("/income-by-source", { preHandler: [app.authenticate] }, getIncomeBySourceReport);
 } 
