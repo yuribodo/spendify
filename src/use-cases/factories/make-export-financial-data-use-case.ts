7d37d190647ca045e17d1a8eeff6d7204124f250
yuribodo <yemotalara@gmail.com>
2025-03-11 21:13:24 -0400

    feat: create a factory to export financial data use case

---
+++ src/use-cases/factories/make-export-financial-data-use-case.ts
@@ -0,0 +1,18 @@
+import { PrismaCategoriesRepository } from "@/repositories/prisma/prisma-categories-repository";
+import { PrismaExpensesRepository } from "@/repositories/prisma/prisma-expenses-repository";
+import { PrismaRevenuesRepository } from "@/repositories/prisma/prisma-revenues-repository";
+import { ExportFinancialDataUseCase } from "../export/export-financial-data";
+
+export function makeExportFinancialDataUseCase() {
+    const expensesRepository = new PrismaExpensesRepository();
+    const revenuesRepository = new PrismaRevenuesRepository();
+    const categoriesRepository = new PrismaCategoriesRepository();
+
+    const exportFinancialDataUseCase = new ExportFinancialDataUseCase(
+        expensesRepository,
+        revenuesRepository,
+        categoriesRepository
+    );
+
+    return exportFinancialDataUseCase;
+} 
