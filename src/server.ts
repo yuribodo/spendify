f97e942807d69597ad5dc7e3fbd128785cacf29b
yuribodo <yemotalara@gmail.com>
2025-03-14 23:47:20 -0400

    build

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
+import { IncomingMessage, ServerResponse } from 'node:http';
 import { app } from "./app";
 import { env } from "./env";
 
-app
-  .listen({
-    host: '0.0.0.0',
-    port: env.PORT,
-  })
-  .then(() => {
-    console.log(`HTTP Server running on port ${env.PORT} 🚀`)
-  })
+if (!process.env.VERCEL) {
+  app
+    .listen({
+      host: '0.0.0.0',
+      port: env.PORT,
+    })
+    .then(() => {
+      console.log(`HTTP Server running on port ${env.PORT} 🚀`);
+    })
+    .catch(err => {
+      console.error("Erro ao iniciar o servidor:", err);
+      process.exit(1);
+    });
+}
+
+export default async (req: IncomingMessage, res: ServerResponse) => {
+  await app.ready();
+  app.server.emit('request', req, res);
+};
