952ae2ebabc25a9506656d0f7f33d96070b32b1a
yuribodo <yemotalara@gmail.com>
2025-03-23 22:56:47 -0400

    feat: add toast to layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Toaster } from "sonner";
 import "./globals.css";
 
 const inter = Inter({
@@ -23,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} font-sans antialiased`}>
         {children}
+        <Toaster />
       </body>
     </html>
   );
