6459407a494968765446edf6deeae7c3829c0c47
yuribodo <yemotalara@gmail.com>
2025-03-27 23:14:41 -0400

    feat: add new get profile route

--- backend/src/http/routes/user.routes.ts
+++ backend/src/http/routes/user.routes.ts
@@ -1,74 +1,124 @@
 import { FastifyInstance } from "fastify";
 import { register } from "../controllers/user/register";
 import { updateProfile } from "../controllers/user/update-user-profile";
+import { getUserProfile } from "../controllers/user/get-user";
 
 export async function userRoutes(app: FastifyInstance) {
-    app.post('/', {
-        schema: {
-            description: 'Route to register a new user',
-            tags: ['Users'],
-            body: {
-                type: 'object',
-                required: ['username', 'email', 'password'],
-                properties: {
-                    username: { type: 'string' },
-                    email: { type: 'string', format: 'email' },
-                    password: { type: 'string', minLength: 6 }
-                }
-            },
-            response: {
-                201: {
-                    description: 'User successfully created',
-                    type: 'object'
-                },
-                409: {
-                    description: 'User already exists',
-                    type: 'object',
-                    properties: {
-                        message: { type: 'string' }
-                    }
-                }
-            }
-        }
-    }, register);
-
-    app.put('/profile', {
-        schema: {
-          description: 'Route to update user profile',
-          tags: ['Users'],
-          security: [{ bearerAuth: [] }],
-          body: {
-            type: 'object',
-            properties: {
-              username: { type: 'string' },
-              email: { type: 'string', format: 'email' },
-              password: { type: 'string', minLength: 6 }
-            }
+  app.post(
+    "/",
+    {
+      schema: {
+        description: "Route to register a new user",
+        tags: ["Users"],
+        body: {
+          type: "object",
+          required: ["username", "email", "password"],
+          properties: {
+            username: { type: "string" },
+            email: { type: "string", format: "email" },
+            password: { type: "string", minLength: 6 },
           },
-          response: {
-            200: {
-              description: 'Profile successfully updated',
-              type: 'object',
-              properties: {
-                user: {
-                  type: 'object',
-                  properties: {
-                    id: { type: 'string' },
-                    username: { type: 'string' },
-                    email: { type: 'string' }
-                  }
-                }
-              }
-            },
-            401: {
-              description: 'Unauthorized',
-              type: 'object',
-              properties: {
-                message: { type: 'string' }
-              }
-            }
-          }
         },
-        preHandler: [app.authenticate]
-    }, updateProfile);
+        response: {
+          201: {
+            description: "User successfully created",
+            type: "object",
+          },
+          409: {
+            description: "User already exists",
+            type: "object",
+            properties: {
+              message: { type: "string" },
+            },
+          },
+        },
+      },
+    },
+    register
+  );
+
+  app.put(
+    "/profile",
+    {
+      schema: {
+        description: "Route to update user profile",
+        tags: ["Users"],
+        security: [{ bearerAuth: [] }],
+        body: {
+          type: "object",
+          properties: {
+            username: { type: "string" },
+            email: { type: "string", format: "email" },
+            password: { type: "string", minLength: 6 },
+          },
+        },
+        response: {
+          200: {
+            description: "Profile successfully updated",
+            type: "object",
+            properties: {
+              user: {
+                type: "object",
+                properties: {
+                  id: { type: "string" },
+                  username: { type: "string" },
+                  email: { type: "string" },
+                },
+              },
+            },
+          },
+          401: {
+            description: "Unauthorized",
+            type: "object",
+            properties: {
+              message: { type: "string" },
+            },
+          },
+        },
+      },
+      preHandler: [app.authenticate],
+    },
+    updateProfile
+  );
+
+  app.get(
+    "/:userId",
+    {
+      schema: {
+        description: "Get user profile",
+        tags: ["Users"],
+        params: {
+          type: "object",
+          required: ["userId"],
+          properties: {
+            userId: { type: "string", format: "uuid" },
+          },
+        },
+        response: {
+          200: {
+            description: "User profile successfully retrieved",
+            type: "object",
+            properties: {
+              user: {
+                type: "object",
+                properties: {
+                  id: { type: "string" },
+                  username: { type: "string" },
+                  email: { type: "string" },
+                },
+              },
+            },
+          },
+          404: {
+            description: "User not found",
+            type: "object",
+            properties: {
+              message: { type: "string" },
+            },
+          },
+        },
+      },
+    },
+    getUserProfile
+  );
 }
