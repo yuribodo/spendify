7f77d46af8a9cbd0ebfa7711574506195c7160da
yuribodo <yemotalara@gmail.com>
2025-03-27 23:17:58 -0400

    feat: store the token in the axios instance

--- frontend/src/lib/axiosInstance.ts
+++ frontend/src/lib/axiosInstance.ts
@@ -1,8 +1,49 @@
 import axios from 'axios';
+import authService from '@/lib/auth/service';
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
 });
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const tokens = localStorage.getItem('user');
+    if (tokens) {
+      const { accessToken } = JSON.parse(tokens);
+      
+      if (accessToken) {
+        config.headers['Authorization'] = `Bearer ${accessToken}`;
+      }
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const newAccessToken = await authService.refreshAccessToken();
+        
+        if (newAccessToken) {
+          originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+          return axiosInstance(originalRequest);
+        }
+      } catch {
+        authService.logout();
+        window.location.href = '/login';
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
